266d4a47c27031e61e3857dff77ec657cd9b18d8
Dario Piotrowicz <dario@cloudflare.com>
2024-05-09 22:19:28 +0100

    implement basic module fallback usage

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 import { Miniflare } from "miniflare";
 
 const mf = new Miniflare({
+  unsafeModuleFallbackService(request) {
+    const resolveMethod = request.headers.get("X-Resolve-Method");
+    if (resolveMethod !== "import" && resolveMethod !== "require") {
+      throw new Error("unrecognized resolvedMethod");
+    }
+
+    const url = new URL(request.url);
+    const specifier = url.searchParams.get("specifier");
+    if (!specifier) {
+      throw new Error("no specifier provided");
+    }
+
+    const name = specifier.replace(/^\//, "");
+    return new Response(
+      JSON.stringify({
+        name,
+        esModule: `
+        export default "A";
+        `,
+      })
+    );
+  },
   workers: [
     {
-      modules: true,
       name: "entrypoint",
-      script: `
-        export default {
-          fetch(req) {
-            return new Response("Hello from Miniflare");
-          }
-        }
-      `,
+      modulesRoot: "/",
+      modules: [
+        {
+          type: "ESModule",
+          path: "/virtual/index.mjs",
+          contents: `
+            export default {
+              async fetch() {
+                const aMod = await import("a");
+                const a = aMod.default;
+                return new Response(a);
+              }
+            }
+          `,
+        },
+      ],
+      unsafeUseModuleFallbackService: true,
     },
   ],
 });
 
-const resp = await mf.dispatchFetch("http://localhost");
+const resp = await mf.dispatchFetch("http://localhost/a");
 
 const text = await resp.text();
 
